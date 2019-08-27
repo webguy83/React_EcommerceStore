fee017b6fa3a8fa1a985150e5b7a14879573863d
Curtis Yacboski <curtisyacboski@gmail.com>
2019-08-27 11:53:01 -0700

    adjust reponsiveness

--- src/components/PhotoCollectionPreview/PhotoCollectionPreview.jsx
+++ src/components/PhotoCollectionPreview/PhotoCollectionPreview.jsx
@@ -7,9 +7,11 @@ export default ({ title, items }) => {
     return (
         <div className="photo-collection-preview">
             <h2 className="photo-collection-preview-title">{title}</h2>
-            {items.filter((_, i) => i < 4).map(({ id, imageUrl, name, price }) => {
-                return <CollectonItem key={id} imageUrl={imageUrl} name={name} price={price} />
-            })}
+            <div className="photo-collection-preview-grid">
+                {items.filter((_, i) => i < 4).map(({ id, imageUrl, name, price }) => {
+                    return <CollectonItem key={id} imageUrl={imageUrl} name={name} price={price} />
+                })}
+            </div>
         </div>
     );
 };
