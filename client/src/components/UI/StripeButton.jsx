a69bd384a9f0bfaa1981b1b1604a0166df8d1a8b
Curtis Yacboski <curtisyacboski@gmail.com>
2019-10-04 00:57:19 -0700

    added client folder and express server

--- client/src/components/UI/StripeButton.jsx
+++ client/src/components/UI/StripeButton.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 import StripeLogo from './logo.svg';
 
 const StripeButton = ({ price }) => {
     const stripePrice = price * 100;
-    const pubKey = "pk_test_qA1t0QAGVZHm4k4dI7Mj0MH0004ZvVtscR"; 
+    const pubKey = "pk_test_qA1t0QAGVZHm4k4dI7Mj0MH0004ZvVtscR";
 
     const onToken = (token) => {
-        console.log(token);
-        console.log('Payment successful!');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: stripePrice,
+                token
+            }
+        }).then(() => {
+            alert('Thank you for your purchase!')
+        }).catch(err => {
+            console.log(`Payment error: ${JSON.parse(err)}`)
+            alert('There was an issue with your payment sorry :(. Please verify your card!')
+        })
     }
 
     return (
